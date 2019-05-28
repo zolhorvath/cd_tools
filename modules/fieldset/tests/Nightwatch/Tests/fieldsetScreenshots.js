42c6d7d2dde2517f73fcbf218054b177004225ed
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-05-28 21:21:16 +0200

    Add Fieldset test (View add form)

---
+++ modules/fieldset/tests/Nightwatch/Tests/fieldsetScreenshots.js
@@ -0,0 +1,35 @@
+/**
+ * @file
+ * Captures Fieldsets.
+ */
+module.exports = {
+  '@tags': ['claro'],
+  before(browser) {
+    'use strict';
+    if (browser.drupalInstall) {
+      browser.drupalInstall({
+        installProfile: 'clarodist'
+      });
+    }
+  },
+  after(browser) {
+    'use strict';
+    if (browser.drupalUninstall) {
+      browser.drupalUninstall().end();
+    }
+    else {
+      browser.end();
+    }
+  },
+  'Fieldset'(browser) {
+    'use strict';
+    ['', 'he'].forEach((langprefix) => {
+      browser
+        .resizeWindow(1024, 600)
+        .smartURL((langprefix ? '/' + langprefix : '') + '/fieldset')
+        .click('[name="page[create]"]')
+        .waitForElementVisible('[name="page[style][style_plugin]"]')
+        .savefullScreenShot('01', langprefix);
+    });
+  }
+};
