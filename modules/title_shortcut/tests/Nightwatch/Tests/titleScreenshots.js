8e00ed469f4b5a90b74777ab60b578d6a2cbc409
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 12:44:44 +0200

    Issue #4: Title test CS

--- modules/title_shortcut/tests/Nightwatch/Tests/titleScreenshots.js
+++ modules/title_shortcut/tests/Nightwatch/Tests/titleScreenshots.js
@@ -3,43 +3,43 @@
  * Captures title with different states.
  */
 module.exports = {
-  '@tags': ['claro'],
+  "@tags": ["claro"],
   before(browser) {
-    'use strict';
     if (browser.drupalInstall) {
       browser.drupalInstall({
-        installProfile: 'clarodist'
+        installProfile: "clarodist"
       });
     }
   },
   after(browser) {
-    'use strict';
     if (browser.drupalUninstall) {
       browser.drupalUninstall().end();
-    }
-    else {
+    } else {
       browser.end();
     }
   },
-  'Page title'(browser) {
-    'use strict';
-    ['', 'he'].forEach((langprefix) => {
+  "Page title": function test(browser) {
+    ["", "he"].forEach(langprefix => {
       browser
         .resizeWindow(1024, 600)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/filter/tips/plain_text')
-        .waitForElementVisible('.shortcut-action__icon')
+        .smartURL(
+          langprefix
+            ? `/${langprefix}/filter/tips/plain_text`
+            : "/filter/tips/plain_text"
+        )
+        .waitForElementVisible(".shortcut-action__icon")
         .pause(250)
-        .saveScreenShot('01', langprefix, 'Page title without shortcut')
-        .focusOn('.shortcut-action')
+        .saveScreenShot("01", langprefix, "Page title without shortcut")
+        .focusOn(".shortcut-action")
         .pause(500)
-        .saveScreenShot('02', langprefix, 'Page title without shortcut focused')
-        .smartURL((langprefix ? '/' + langprefix : '') + '/filter/tips')
-        .waitForElementVisible('.shortcut-action__icon')
+        .saveScreenShot("02", langprefix, "Page title without shortcut focused")
+        .smartURL(langprefix ? `/${langprefix}/filter/tips` : "/filter/tips")
+        .waitForElementVisible(".shortcut-action__icon")
         .pause(250)
-        .saveScreenShot('03', langprefix, 'Page title with shortcut')
-        .focusOn('.shortcut-action')
+        .saveScreenShot("03", langprefix, "Page title with shortcut")
+        .focusOn(".shortcut-action")
         .pause(500)
-        .saveScreenShot('04', langprefix, 'Page title with shortcut focused');
+        .saveScreenShot("04", langprefix, "Page title with shortcut focused");
     });
   }
 };
