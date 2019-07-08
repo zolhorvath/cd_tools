f9a5d96121c74fcf1bd923f0937f7225dfc17cf0
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 15:30:35 +0200

    Issue #4: Progress test module CS

--- modules/progress/tests/Nightwatch/Tests/progressScreenshots.js
+++ modules/progress/tests/Nightwatch/Tests/progressScreenshots.js
@@ -3,37 +3,38 @@
  * Captures progress and throbbers with different states.
  */
 module.exports = {
-  '@tags': ['claro'],
+  "@tags": ["claro"],
   before(browser) {
-    'use strict';
     if (browser.drupalInstall) {
       browser.drupalInstall({
-        installProfile: 'clarodist'
+        installProfile: "clarodist"
       });
     }
   },
   after(browser) {
-    'use strict';
     if (browser.drupalUninstall) {
       browser.drupalUninstall().end();
-    }
-    else {
+    } else {
       browser.end();
     }
   },
-  'Progress'(browser) {
-    'use strict';
-    ['', 'he'].forEach((langprefix) => {
+  Progress(browser) {
+    ["", "he"].forEach(langprefix => {
       browser
         .resizeWindow(1024, 600)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/progress')
-        .waitForElementPresent('.ajax-progress.progress--small', 5000)
-        .waitForElementPresent('.ajax-progress.progress', 5000)
-        .savefullScreenShot('01', langprefix)
-        .click('[name="button_visible"]').pause(400)
-        .savefullScreenShot('02', langprefix)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/progress/fullscreen')
-        .savefullScreenShot('03', langprefix);
+        .smartURL(langprefix ? `/${langprefix}/progress` : "/progress")
+        .waitForElementPresent(".ajax-progress.progress--small", 5000)
+        .waitForElementPresent(".ajax-progress.progress", 5000)
+        .savefullScreenShot("01", langprefix)
+        .click('[name="button_visible"]')
+        .pause(400)
+        .savefullScreenShot("02", langprefix)
+        .smartURL(
+          langprefix
+            ? `/${langprefix}/progress/fullscreen`
+            : "/progress/fullscreen"
+        )
+        .savefullScreenShot("03", langprefix);
     });
   }
 };
