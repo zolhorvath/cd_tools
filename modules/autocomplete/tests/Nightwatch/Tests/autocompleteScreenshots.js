c7a2585921de45dc905c0dd0a9e50c7a4f39d16f
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 16:34:44 +0200

    Issue #4: Autocomplete test CS

--- modules/autocomplete/tests/Nightwatch/Tests/autocompleteScreenshots.js
+++ modules/autocomplete/tests/Nightwatch/Tests/autocompleteScreenshots.js
@@ -3,54 +3,63 @@
  * Captures autocomplete with different states.
  */
 module.exports = {
-  '@tags': ['claro'],
+  "@tags": ["claro"],
   before(browser) {
-    'use strict';
     if (browser.drupalInstall) {
       browser.drupalInstall({
-        installProfile: 'clarodist'
+        installProfile: "clarodist"
       });
     }
   },
   after(browser) {
-    'use strict';
     if (browser.drupalUninstall) {
       browser.drupalUninstall().end();
-    }
-    else {
+    } else {
       browser.end();
     }
   },
-  'Autocomplete'(browser) {
-    'use strict';
-    ['', 'he'].forEach((langprefix) => {
+  Autocomplete(browser) {
+    ["", "he"].forEach(langprefix => {
       browser
         .resizeWindow(1024, 600)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/autocomplete')
-        .setValue('[name="country_autocomplete"]', 'td')
-        .waitForElementVisible('.ui-autocomplete:first-of-type', 5000)
-        .click('.ui-autocomplete:first-of-type .ui-menu-item-wrapper:last-child')
-        .setValue('[name="country_autocomplete"]', ',').pause(400)
-        .setValue('[name="country_autocomplete"]', 'do')
-        .waitForElementVisible('.ui-autocomplete:first-of-type', 5000)
-        .click('.ui-autocomplete:first-of-type .ui-menu-item-wrapper:last-child')
-        .setValue('[name="country_autocomplete"]', ',').pause(400)
-        .setValue('[name="country_autocomplete"]', ' ').pause(400)
-        .setValue('[name="country_autocomplete"]', 'a')
-        .savefullScreenShot('01', langprefix)
-        .waitForElementVisible('.ui-autocomplete:first-of-type', 5000)
-        .savefullScreenShot('02', langprefix)
-        .setValue('[name="autocomplete_multipe[0][country]"]', 'gf')
-        .waitForElementVisible('.ui-autocomplete:last-of-type', 5000)
-        .click('.ui-autocomplete:last-of-type .ui-menu-item-wrapper:last-child')
+        .smartURL(langprefix ? `/${langprefix}/autocomplete` : "/autocomplete")
+        .setValue('[name="country_autocomplete"]', "td")
+        .waitForElementVisible(".ui-autocomplete:first-of-type", 5000)
+        .click(
+          ".ui-autocomplete:first-of-type .ui-menu-item-wrapper:last-child"
+        )
+        .setValue('[name="country_autocomplete"]', ",")
+        .pause(400)
+        .setValue('[name="country_autocomplete"]', "do")
+        .waitForElementVisible(".ui-autocomplete:first-of-type", 5000)
+        .click(
+          ".ui-autocomplete:first-of-type .ui-menu-item-wrapper:last-child"
+        )
+        .setValue('[name="country_autocomplete"]', ",")
+        .pause(400)
+        .setValue('[name="country_autocomplete"]', " ")
+        .pause(400)
+        .setValue('[name="country_autocomplete"]', "a")
+        .savefullScreenShot("01", langprefix)
+        .waitForElementVisible(".ui-autocomplete:first-of-type", 5000)
+        .savefullScreenShot("02", langprefix)
+        .setValue('[name="autocomplete_multipe[0][country]"]', "gf")
+        .waitForElementVisible(".ui-autocomplete:last-of-type", 5000)
+        .click(".ui-autocomplete:last-of-type .ui-menu-item-wrapper:last-child")
         .click('[name="claro-autocomplete-country_add_more"]')
-        .waitForElementVisible('[name="autocomplete_multipe[1][country]"]', 5000)
-        .setValue('[name="autocomplete_multipe[1][country]"]', 'ee')
-        .waitForElementVisible('.ui-autocomplete:last-of-type', 5000)
-        .click('.ui-autocomplete:last-of-type .ui-menu-item-wrapper:last-child')
+        .waitForElementVisible(
+          '[name="autocomplete_multipe[1][country]"]',
+          5000
+        )
+        .setValue('[name="autocomplete_multipe[1][country]"]', "ee")
+        .waitForElementVisible(".ui-autocomplete:last-of-type", 5000)
+        .click(".ui-autocomplete:last-of-type .ui-menu-item-wrapper:last-child")
         .click('[name="claro-autocomplete-country_add_more"]')
-        .waitForElementVisible('[name="autocomplete_multipe[2][country]"]', 5000)
-        .savefullScreenShot('03', langprefix);
+        .waitForElementVisible(
+          '[name="autocomplete_multipe[2][country]"]',
+          5000
+        )
+        .savefullScreenShot("03", langprefix);
     });
   }
 };
