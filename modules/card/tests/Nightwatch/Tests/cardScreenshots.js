c25968e7030fb0ec0a01ad885b832fca4cfa9bcd
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 12:40:01 +0200

    Issue #2: Add Card component test module

---
+++ modules/card/tests/Nightwatch/Tests/cardScreenshots.js
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * Captures Card component on system appearance page.
+ */
+module.exports = {
+  "@tags": ["claro"],
+  before(browser) {
+    if (browser.drupalInstall) {
+      browser.drupalInstall({
+        installProfile: "clarodist"
+      });
+    }
+  },
+  after(browser) {
+    if (browser.drupalUninstall) {
+      browser.drupalUninstall().end();
+    } else {
+      browser.end();
+    }
+  },
+  Card: function test(browser) {
+    ["", "he"].forEach(langprefix => {
+      browser
+        .resizeWindow(1024, 600)
+        .smartURL(
+          langprefix ? `/${langprefix}/admin/appearance` : "/admin/appearance"
+        )
+        .savefullScreenShot();
+    });
+  }
+};
