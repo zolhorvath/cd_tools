9b12cffa2211ee5674fd150387ceb3a25b002e88
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 16:29:04 +0200

    Issue #4: Button test CS

--- modules/button/tests/Nightwatch/Tests/buttonScreenshots.js
+++ modules/button/tests/Nightwatch/Tests/buttonScreenshots.js
@@ -3,52 +3,63 @@
  * Captures buttons with different states.
  */
 module.exports = {
-  '@tags': ['claro'],
+  "@tags": ["claro"],
   before(browser) {
-    'use strict';
     if (browser.drupalInstall) {
       browser.drupalInstall({
-        installProfile: 'clarodist'
+        installProfile: "clarodist"
       });
     }
   },
   after(browser) {
-    'use strict';
     if (browser.drupalUninstall) {
       browser.drupalUninstall().end();
-    }
-    else {
+    } else {
       browser.end();
     }
   },
-  'Buttons'(browser) {
-    'use strict';
-    ['', 'he'].forEach((langprefix) => {
+  Buttons(browser) {
+    ["", "he"].forEach(langprefix => {
       browser
         .resizeWindow(1024, 600)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/buttons')
-        .waitForElementVisible('.sbs-layout__region--main', 5000)
-        .elements('css selector', '.sbs-layout__region--main .button', (result) => {
-          let i = 2;
-          result.value.forEach(elem => {
-            browser.perform(done => {
-              i++;
-              browser.elementIdAttribute(elem.ELEMENT, 'id', result => {
-                if (result.value) {
-                  browser
-                    .focusOn('#' + result.value)
-                    .pause(100)
-                    .saveScreenShot(i.toString().padStart(2, '0'), langprefix);
-                }
+        .smartURL(langprefix ? `/${langprefix}/buttons` : "/buttons")
+        .waitForElementVisible(".sbs-layout__region--main", 5000)
+        .elements(
+          "css selector",
+          ".sbs-layout__region--main .button",
+          mainButtonsQueryResult => {
+            let index = 2;
+            /* eslint-disable max-nested-callbacks */
+            mainButtonsQueryResult.value.forEach(elem => {
+              browser.perform(done => {
+                index += 1;
+                browser.elementIdAttribute(
+                  elem.ELEMENT,
+                  "id",
+                  buttonIdResult => {
+                    if (buttonIdResult.value) {
+                      browser
+                        .focusOn(`#${buttonIdResult.value}`)
+                        .pause(100)
+                        .saveScreenShot(
+                          index.toString().padStart(2, "0"),
+                          langprefix
+                        );
+                    }
+                  }
+                );
+                done();
               });
-              done();
             });
-          });
-        })
-        .savefullScreenShot('01', langprefix)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/buttons/disabled')
-        .waitForElementVisible('.sbs-layout__region--main', 5000)
-        .savefullScreenShot('02', langprefix);
+            /* eslint-enable max-nested-callbacks */
+          }
+        )
+        .savefullScreenShot("01", langprefix)
+        .smartURL(
+          langprefix ? `/${langprefix}/buttons/disabled` : "/buttons/disabled"
+        )
+        .waitForElementVisible(".sbs-layout__region--main", 5000)
+        .savefullScreenShot("02", langprefix);
     });
   }
 };
