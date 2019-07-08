ef761d4f021ee18dacab6cf9c93b0ca9d6f707c9
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-08 15:12:37 +0200

    Issue #4: Select test CS

--- modules/select/tests/Nightwatch/Tests/selectScreenshots.js
+++ modules/select/tests/Nightwatch/Tests/selectScreenshots.js
@@ -5,40 +5,48 @@
  * Error state's fileName vary by the state of the inline_form_errors module.
  */
 module.exports = {
-  '@tags': ['claro'],
+  "@tags": ["claro"],
   before(browser) {
-    'use strict';
     if (browser.drupalInstall) {
       browser.drupalInstall({
-        installProfile: 'clarodist'
+        installProfile: "clarodist"
       });
     }
   },
   after(browser) {
-    'use strict';
     if (browser.drupalUninstall) {
       browser.drupalUninstall().end();
-    }
-    else {
+    } else {
       browser.end();
     }
   },
-  'Select form'(browser) {
-    'use strict';
-    ['', 'he'].forEach((langprefix) => {
+  "Select form": function test(browser) {
+    ["", "he"].forEach(langprefix => {
       browser
         .resizeWindow(1024, 600)
-        .smartURL((langprefix ? '/' + langprefix : '') + '/contact/select')
+        .smartURL(
+          langprefix ? `/${langprefix}/contact/select` : "/contact/select"
+        )
         .click('[name="select_single"]')
         .click('[name="select_single"] option[value="error"]')
         .click('[name="select_multiple[]"] option[value="error"]')
         .pause(300)
-        .savefullScreenShot('01', langprefix)
-        .click('input#edit-submit')
-        .waitTillElementPresent('.messages--error', 5000)
-        .elements('css selector', '.form-item__error-message,.form-item--error-message', (results) => {
-          browser.savefullScreenShot((results.value.length ? '03' : '02'), langprefix, (results.value.length ? 'Select form with inline errors' : 'Select form with errors'));
-        });
+        .savefullScreenShot("01", langprefix)
+        .click("input#edit-submit")
+        .waitTillElementPresent(".messages--error", 5000)
+        .elements(
+          "css selector",
+          ".form-item__error-message,.form-item--error-message",
+          inlineFormMessagesQueryResults => {
+            browser.savefullScreenShot(
+              inlineFormMessagesQueryResults.value.length ? "03" : "02",
+              langprefix,
+              inlineFormMessagesQueryResults.value.length
+                ? "Select form with inline errors"
+                : "Select form with errors"
+            );
+          }
+        );
     });
   }
 };
