13341b67b5b66423c6629ac6352fd154338c0875
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-07-11 14:35:12 +0200

    Minor enhancements for password testing.

--- modules/password/tests/Nightwatch/Tests/passwordScreenshots.js
+++ modules/password/tests/Nightwatch/Tests/passwordScreenshots.js
@@ -25,27 +25,33 @@ module.exports = {
         .smartURL(langprefix ? `/${langprefix}/password` : "/password")
         .waitForElementPresent('[name="password_confirm[pass1]"]', 5000)
         .waitForElementPresent('[name="password_confirm[pass2]"]', 5000)
+        .savefullScreenShot("01", langprefix, "Initial state")
         // Pw1.
         .setValue('[name="password_confirm[pass1]"]', "ascii")
+        .pause(100)
+        .waitForElementVisible('[name="password_confirm[pass2]"]', 5000)
         // Pw2.
         .setValue('[name="password_confirm[pass2]"]', "ascii")
-        .savefullScreenShot("01", langprefix, "Zero but same")
+        .savefullScreenShot("02", langprefix, "Zero but same")
         // Pw1.
         .setValue('[name="password_confirm[pass1]"]', "asc")
         .pause(1000)
-        .savefullScreenShot("02", langprefix, "Weak")
+        .savefullScreenShot("03", langprefix, "Weak")
         .setValue('[name="password_confirm[pass1]"]', "iias")
         .pause(1000)
-        .savefullScreenShot("03", langprefix, "Fair")
+        .savefullScreenShot("04", langprefix, "Fair")
         .setValue('[name="password_confirm[pass1]"]', "!")
         .pause(1000)
-        .savefullScreenShot("04", langprefix, "Medium")
+        .savefullScreenShot("05", langprefix, "Medium")
         .setValue('[name="password_confirm[pass1]"]', "E") // Uppercase utf8 doesn't mean an uppercase char?!
         .pause(1000)
-        .savefullScreenShot("05", langprefix, "Strong")
+        .savefullScreenShot("06", langprefix, "Strong")
         .setValue('[name="password_confirm[pass1]"]', "4")
         .pause(1000)
-        .savefullScreenShot("06", langprefix, "Super");
+        .savefullScreenShot("07", langprefix, "Super")
+        .click('[type="submit"]')
+        .pause(100)
+        .savefullScreenShot("08", langprefix, "Error");
     });
   }
 };
