553c35dc1c5d8bd9eced45b9d9e815aa59b3afdf
Zoltan Horvath <zoltan.horvath@cheppers.com>
2019-08-29 17:08:34 +0200

    Issue #8: Extend Message component test with JavaScript theme examples

---
+++ modules/message/js/message-test.js
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * Provides test markup for Drupal.theme.message callback.
+ */
+
+/* eslint-env es6:false, node:false */
+/* eslint-disable strict, func-names, object-shorthand, no-var, prefer-template, prefer-arrow-callback */
+(function($, Drupal) {
+  "use strict";
+
+  $.extend(Drupal.Message, {
+    getMessageTypeLabels() {
+      return {
+        status: Drupal.t("Status message"),
+        error: Drupal.t("Error message"),
+        warning: Drupal.t("Warning message"),
+        // Our additions.
+        custom: Drupal.t("Custom message"),
+        info: Drupal.t("Info message")
+      };
+    }
+  });
+
+  Drupal.behaviors.messageTest = {
+    attach: function(context, settings) {
+      $("body", context)
+        .once("messageTest")
+        .each(function() {
+          var messenger = new Drupal.Message();
+
+          Object.keys(settings.message || {}).forEach(function(messageType) {
+            if (settings.message[messageType]) {
+              messenger.add(settings.message[messageType], {
+                type: messageType
+              });
+            }
+          });
+        });
+    }
+  };
+})(jQuery, Drupal);
+/* eslint-enable strict, func-names, object-shorthand, no-var, prefer-template, prefer-arrow-callback */
+/* eslint-env es6:true, node:true */
--- modules/message/src/Controller/MessageController.php
+++ modules/message/src/Controller/MessageController.php
@@ -12,13 +12,17 @@ class MessageController extends ControllerBase {
   /**
    * Displays page with test messages.
    *
-   * @param string $length
+   * @param string $type
    *   Length of the messages: short or long.
    *
    * @return array
    *   A render array for the test page with messages.
    */
-  public function messagePage($length) {
+  public function messagePage($type) {
+    $build = [
+      '#type' => 'markup',
+      '#markup' => $this->t('Messages'),
+    ];
     $message_types = [
        // This is a custom type.
       'custom' => $this->t('Custom type message'),
@@ -29,29 +33,46 @@ class MessageController extends ControllerBase {
       'error' => $this->t('Error message'),
     ];
 
-    $recommendations = [
+    $recommendations_items = [
       $this->t('Make it at least 12 characters'),
       $this->t('Add lowercase letters'),
       $this->t('Add uppercase letters'),
       $this->t('Add numbers'),
       $this->t('Add punctuation'),
     ];
-
-    foreach ($message_types as $message_type => $message) {
-      $this->messenger()->addMessage($message, $message_type);
-
-      if ($length === 'long') {
-        $this->messenger()->addMessage($this->t('Recommendations to make your password stronger:'), $message_type);
-        foreach ($recommendations as $recommendation) {
-          $this->messenger()->addMessage($recommendation, $message_type);
-        }
-      }
+    $recommendations = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+    ];
+    foreach ($recommendations_items as $recommendations_item) {
+      $recommendations['#items'][] = ['#markup' => $recommendations_item];
     }
 
-    return [
-      '#type' => 'markup',
-      '#markup' => $this->t('Messages'),
-    ];
+    switch ($type) {
+      case 'js':
+        $build['#attached']['library'][] = 'message/message-test';
+        foreach (array_keys($message_types) as $message_type) {
+          $build['#attached']['drupalSettings']['message'][$message_type] = $this->t('Some background about <a href=":wiki_url_password">passwords</a>.', [
+            ':wiki_url_password' => 'https://en.wikipedia.org/wiki/Password',
+          ]);
+        }
+        break;
+
+      default:
+        foreach ($message_types as $message_type => $message) {
+          $this->messenger()->addMessage($message, $message_type);
+
+          if ($type === 'long') {
+            $this->messenger()->addMessage($this->t('Recommendations to make your <a href=":wiki_url_password_strength">password</a> stronger:', [
+              ':wiki_url_password_strength' => 'https://en.wikipedia.org/wiki/Password_strength#Usability_and_implementation_considerations',
+            ]), $message_type);
+            $this->messenger()->addMessage($recommendations, $message_type);
+          }
+        }
+        break;
+    }
+
+    return $build;
   }
 
 }
